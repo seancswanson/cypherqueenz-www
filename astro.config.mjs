94a5a6e8424406e2a10308dda4fc4af234a2521b
Sean Swanson <seancswan@gmail.com>
2023-01-02 23:04:24 -0800

    Added in Astro plugins that will optimize and improve this app's performance and SEO.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://naka.studio',
+  integrations: [image(), tailwind(), prefetch(), sitemap(), compress()]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				skinShadow: 'rgb(var(--color-shadow) / <alpha-value>)'
+			},
+			fontFamily: {
+				skinDisplay: 'var(--font-family-skin-display), serif',
+				skinMono: "'Space Mono', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace",
+
+			},
+			backgroundColor: {
+				skin: {
+					base: 'var(--color-bg-base)',
+					accent: 'var(--color-bg-accent)',
+				}
+			},
+			backgroundImage: {
+				skinPattern: 'var(--bg-image-skin-pattern)'
+			},
+			backgroundSize: {
+				skinPattern: 'var(--bg-image-skin-pattern-size)'
+			},
+			minHeight: {
+				'50': '50px',
+				'160': '160px'
+			},
+			maxHeight: {
+				'160': '160px'
+			},
+			minWidth: {
+				'475': '475px'
+			},
+			maxWidth: {
+				'475': '475px'
+			},
+			textColor:
+			{
+				skin: {
+					base: 'var(--color-base)',
+				}
+			}
+		},
+	},
+	plugins: [],
+}
