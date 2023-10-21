a3c6440a8d19325823b09e33f62781384fda8bfa
Sean Swanson <seancswan@gmail.com>
2023-10-21 14:48:58 -0700

    Added boilerplate netlify function file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import image from "@astrojs/image";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 import compress from "astro-compress";
 
-// https://astro.build/config
-
-// https://astro.build/config
 import sanity from "astro-sanity";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://cypherqueenz.com",
   integrations: [
@@ -32,7 +23,7 @@ export default defineConfig({
       projectId: "k34rqpdo",
       dataset: "production_www",
       apiVersion: "2021-03-25",
-      useCdn: true
+      useCdn: true,
     }),
     compress(),
   ],
